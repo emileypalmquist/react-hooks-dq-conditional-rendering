450a8afa09220920665cca3e5b83fc606535ca1c
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2022-06-13 17:22:33 -0700

    review solution

--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuBar from "./MenuBar";
 import { Profile, Photos, Cocktails, Pokemon } from "./pages";
 
 function MainBox() {
-  /*
-    Replace the code below! Depending on what menu item is selected in the menu, 
-    I should render either a Profile, Photos, Cocktails, or Pokemon component.
-    Think of a way to track which menu item was selected. 
-    - Which component should have state? 
-    - Which component should have methods to control state? 
-    - Where should these methods be called?
-  */
+  const [activeIcon, setActiveIcon] = useState("profile");
 
-  let detailsToDisplay = <div>Hi, I'm a div!</div>;
+  // let detailsToDisplay = () => {
+  //   if (activeIcon === "profile") {
+  //     return <Profile />;
+  //   } else if (activeIcon === "photos") {
+  //     return <Photos />;
+  //   } else if (activeIcon === "cocktails") {
+  //     return <Cocktails />;
+  //   } else if (activeIcon === "pokemon") {
+  //     return <Pokemon />;
+  //   }
+  // };
+  let details = {
+    profile: <Profile />,
+    photos: <Photos />,
+    cocktails: <Cocktails />,
+    pokemon: <Pokemon />,
+  };
 
   return (
     <div>
-      <MenuBar />
-      {detailsToDisplay}
+      <MenuBar activeIcon={activeIcon} setActiveIcon={setActiveIcon} />
+      {details[activeIcon]}
     </div>
   );
 }
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,32 +1,35 @@
 import React from "react";
 
-function MenuBar(props) {
-  /*
-
-  The 'span' tags below are the menu items. Think about the way a menu 
-  should work. When you click a menu item, the button typically becomes
-  'active' to indicate that it is currently selected. How could we achieve
-  this programatically? What other behavior do we expect when we click
-  on a menu item? Do we need state in this component, and if not, how can
-  this component be made aware of what is currently the active menu item?
-
-  */
+function MenuBar({ activeIcon, setActiveIcon }) {
+  const className = (icon) => (activeIcon === icon ? "item active" : "item");
 
   return (
     <div className="ui four item menu">
-      <span className="item active">
+      <span
+        className={className("profile")}
+        onClick={() => setActiveIcon("profile")}
+      >
         <i className="user large icon" />
       </span>
 
-      <span className="item">
+      <span
+        className={className("photos")}
+        onClick={() => setActiveIcon("photos")}
+      >
         <i className="photo large icon" />
       </span>
 
-      <span className="item">
+      <span
+        className={className("cocktails")}
+        onClick={() => setActiveIcon("cocktails")}
+      >
         <i className="cocktail large icon" />
       </span>
 
-      <span className="item">
+      <span
+        className={className("pokemon")}
+        onClick={() => setActiveIcon("pokemon")}
+      >
         <i className=" themeisle large icon" />
       </span>
     </div>
